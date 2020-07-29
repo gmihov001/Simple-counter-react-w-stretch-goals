5e5711fd7534b0651210dff479708bfbd34a4ec5
George Mihov <gmihov001@gmail.com>
2020-07-29 15:36:30 +0000

    Updates

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,11 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
 
 //create your first component
 export function Home(props) {
 	return (
 		<div className="container-fluid text-center mt-5">
-			<div>clock</div>
+			<div>
+				<i className="far fa-clock" />
+			</div>
 			<div>{props.num1}</div>
 			<div>{props.num2}</div>
 			<div>{props.num3}</div>
